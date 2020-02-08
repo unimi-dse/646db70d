57e46462433b24a5c435067508f23b894c32b96d
rluca96 <10492455@polimi.it>
2020-02-08 12:12:54 +0100

    removed pipe in plotly

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -19,14 +19,13 @@ shiny::shinyServer(function(input, output){
     colnames(historical) <- c("Date", "Open", "High", "Low", "Close", "Volume", "Adjusted")
 
     #Plots candlestick graph#
-    plotly::plot_ly( x = historical$Date, type="candlestick",
-                     open = historical$Open, close = historical$Close,
-                     high = historical$High, low = historical$Low) %>%
 
       plotly::add_lines(x = historical$Date, y= historical$Open,
-                line = list(width = 0.75), inherit = F) %>%
+                line = list(width = 0.75), inherit = F, plotly::layout(xaxis = list (title = "Dates", zeroline = FALSE),
+                                                                        yaxis = list(title = "Price"), plotly::plot_ly( x = historical$Date, type="candlestick",
+                                                                                                                         open = historical$Open, close = historical$Close,
+                                                                                                                         high = historical$High, low = historical$Low)))
 
-      plotly::layout(xaxis = list (title = "Dates", zeroline = FALSE),
-             yaxis = list(title = "Price"))
+
   })
 })
